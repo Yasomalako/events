d77173d77a2545b7672434979c51805535f50619
Yasomalako <Yasomalako@gmail.com>
2022-06-02 09:03:00 +0300

    objects events

---
+++ main.js
@@ -0,0 +1,67 @@
+// function some(){
+//     // alert("Hi there");
+//     var x = document.getElementById('para').innerHTML= 'itzlahti';}
+//    document.getElementById("para").style.color="blue"
+// function boss(){
+//     var x = document.getElementsByClassName('.xox').innerHTML= 'yaso the boss';}
+// function beast(){
+//     document.getElementsByTagName('h2').innerHTML= 'is the beast';}
+//1/
+// function printAlert(){
+//     alert("u made it")
+// }
+//2//
+// function printMyname(){
+//     console.log("yaso")
+// }
+//3//
+// function printMyname(){
+//     var color = document.getElementById("blue");
+//     color.style.color="blue"
+// }
+//4//
+// function chengeh1(){
+// var size = document.getElementById('blu');
+// size.style.height='100px'
+// }
+//5//
+// function popay(){
+//     var chenge = document.getElementById('first');
+//     chenge.innerText= 'shalom'
+//     console.log('nnqw')
+// }
+//6//
+// function printUserName(){
+// var printer = document.getElementById("put");
+// var parag = document.getElementById("par")
+// parag.innerHTML= printer.value
+
+// };
+
+// function name(){
+// var button = document.getElementById("btn");
+
+// }
+//7//
+function dateObject(){
+    
+    var Date={
+        whatisthetime:prompt("what is the time")
+
+    };
+        if(whatisthetime<12){
+          var color1= document.getElementsByTagName('body');
+          color2.style.bgcolor = "yellow"
+          var paragraph = document.getElementById("para");
+          paragraph.innerText = 'have a good day'
+        }
+        else{
+            var color2 = document.getElementById("para");
+            color2.style.bgcolor = "blue"
+            paragraph.innerText='have agood afternoon'
+            
+
+        }
+      
+    }
+
